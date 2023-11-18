8b4209099c6a8b54ba92f9a9367e49e2aa20b618
jujuwon <webmon@kakao.com>
2023-11-18 21:58:11 +0900

    feat: init project

---
+++ src/main/kotlin/kr/ac/changwon/together/TogetherApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.changwon.together
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TogetherApplication
+
+fun main(args: Array<String>) {
+    runApplication<TogetherApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/changwon/together/TogetherApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.changwon.together
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TogetherApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
