409724b03c7b72055f091bf7905583201c8b88b8
jujuwon <webmon@kakao.com>
2023-11-30 02:19:26 +0900

    docs: 로그인/회원가입 API 명세 추가

--- src/main/kotlin/kr/ac/changwon/together/auth/controller/AuthController.kt
+++ src/main/kotlin/kr/ac/changwon/together/auth/controller/AuthController.kt
@@ -16,12 +16,12 @@ import javax.validation.Valid
 class AuthController(
     private val service: AuthService
 ) {
-    @ApiOperation(value = "회원가입", notes = "회원가입 성공시 JWT를 반환합니다.")
+    @ApiOperation(value = "회원가입 (부분완료) 기본 이미지 등록 필요", notes = "회원가입 성공시 JWT를 반환합니다.")
     @PostMapping("/sign-up")
     fun signUp(@Valid @RequestBody req: ReqSignUp): RestApiResponse<ResLoginToken> =
         RestApiResponse.success(service.signUp(req = req))
 
-    @ApiOperation(value = "로그인", notes = "로그인 성공시 JWT를 반환합니다.")
+    @ApiOperation(value = "로그인 (완료)", notes = "로그인 성공시 JWT를 반환합니다.")
     @GetMapping("/sign-in")
     fun signIn(@Valid req: ReqSignIn): RestApiResponse<ResLoginToken> =
         RestApiResponse.success(service.signIn(req = req))
