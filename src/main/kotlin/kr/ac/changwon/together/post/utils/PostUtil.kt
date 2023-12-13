da4158452b23e4417331b8848a1ef7ec8d2516ed
jujuwon <webmon@kakao.com>
2023-12-13 20:03:21 +0900

    fix: 댓글 조회 시 StackOverflow 오류 수정

--- src/main/kotlin/kr/ac/changwon/together/post/utils/PostUtil.kt
+++ src/main/kotlin/kr/ac/changwon/together/post/utils/PostUtil.kt
@@ -5,7 +5,11 @@ import kr.ac.changwon.together.post.vo.CommentDto
 
 // 댓글 정보 가져오기
 fun Comment.mapCommentToDto(): CommentDto {
-    val subComments = this.comments.map { mapCommentToDto() }
+    val subComments = if (this.comments.isEmpty()) {
+           emptyList()
+       } else {
+           this.comments.map { it.mapCommentToDto() }
+       }
     return CommentDto(
         userId = this.user.id!!,
         nickname = this.user.nickname,
